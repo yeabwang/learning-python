cc2070d851eefb2d1f4f201903d3ce1d8d2b7ffc
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-11-28 04:16:41 +0800

    Updating

---
+++ calculator.py
@@ -0,0 +1,11 @@
+x = float(input("Whats the value of x: "))
+y = float(input("Whats the value of y: "))
+
+# Rounding numbers
+# Currently the limit on int numbers is removed so they can be as big as we want them to be 
+s = round(x+y)
+d = x/y
+
+# :, - This will do a good formatting by separating every thousand values by a comma 
+print(f"The addition answer is {s:,}")
+print(f"The division answer is {d}")
---
+++ dotstring.py
@@ -0,0 +1,16 @@
+# def exampleFunction():
+#     """
+#     This is a google doc style string.
+    
+#     Args:
+#         a do something.
+#         b do something.
+    
+#     returns:
+#         will return something.
+    
+#     """
+#     return 0
+
+# print(exampleFunction.__doc__)
+
