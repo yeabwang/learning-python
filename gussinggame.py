23f59481b0b3a153433ccd635974bfcd6b4426c0
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-11-28 21:01:32 +0800

    gussing game

---
+++ gussinggame.py
@@ -0,0 +1,15 @@
+import random
+luckyNumber = random.randint(1,10)
+numberOfTrials = 3
+trialCount = 0
+
+while trialCount < numberOfTrials:
+    guess = int(input("Guess the number: "))
+    if(guess == luckyNumber):
+        print(f"Wow! You got the lucky number, {luckyNumber} in {numberOfTrials} number of trials")
+        break
+    else:
+        print(f"Your guess {guess} is wrong, try again.")
+    trialCount +=1
+    
+print(f"The lucky number was {luckyNumber}")       
