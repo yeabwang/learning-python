3b98be54bda4334874e0dd77c6c46b0c2c2518b1
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-11-28 19:37:12 +0800

    arthimetic

---
+++ arthimetic.py
@@ -0,0 +1,9 @@
+# // for floor division
+# ** for powers
+# round(x) for rounding
+# abs for converting the value to positive
+# we can import the module math to access extra mathmatical functions
+import math as m
+
+x = -3.4
+print(m.ceil(x))
