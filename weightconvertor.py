c5367a89b24634fa92fb869ffcc0e6debac643ea
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-11-28 18:55:19 +0800

    type conversion

---
+++ weightconvertor.py
@@ -0,0 +1,5 @@
+# pound to kg == 1 lb = 0.4 kg
+
+weightInPounds = float(input("Enter your weight in pounds: "))
+weightInKg = 0.453 * weightInPounds
+print(f"Your weight is equal to {weightInKg}Kg.")
