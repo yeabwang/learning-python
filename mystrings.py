249cf9b93cbc21919d2dae0a54f443a4165f1c48
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-11-28 19:19:05 +0800

    strings

---
+++ mystrings.py
@@ -0,0 +1,11 @@
+# We  can define strings in single, double and triple qoutes(for multilines)
+# Singe its a collection of char we can access them indiviually using [index]
+# It allows negative indexing
+# Slicing works by [: ]
+
+# firstName = "Jhon"
+# lastName = "Smith"
+# print(f"{firstName} [{lastName}] is a coder")
+course = "Python for Beginners"
+print(course.replace("P", "J"))
+print("Jython" in course)
